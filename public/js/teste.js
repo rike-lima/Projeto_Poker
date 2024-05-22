811f2ac5f1f5260d78e22df4c02e714ce7c2f11f
rike-lima <rodolfo.lima@sptech.school>
2024-05-22 00:14:36 -0300

    Alterações na página de home e adição de imagens

--- public/js/teste.js
+++ public/js/teste.js
@@ -1,6 +1,21 @@
 const nav = document.querySelector(".menuContent");
 
-
-
 window.addEventListener("scroll", () => nav.classList.toggle("active", scrollY > 0));
+
+
+var count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+},4000)
+
+function nextImage(){
+    count++;
+    if (count>4){
+        count = 1;
+        
+    }
+    document.getElementById("radio" +count).checked = true;
+}
 
