94cbd1c3a12a999f167934c51bcec9b59fbb828f
rike-lima <rodolfo.lima@sptech.school>
2024-05-24 17:55:26 -0300

    alterações nas última duas sessóes da página home

--- public/js/teste.js
+++ public/js/teste.js
@@ -2,20 +2,29 @@ const nav = document.querySelector(".menuContent");
 
 window.addEventListener("scroll", () => nav.classList.toggle("active", scrollY > 0));
 
+var img1 = `./assets/imagens/WSOP_Logo.jpg`
 
-var count = 1;
-document.getElementById("radio1").checked = true;
 
-setInterval( function(){
-    nextImage();
-},4000)
+var listaEventos=[img1, `./assets/imagens/ept_logo.jpg`,`./assets/imagens/PCA_logo.jpg`,`./assets/imagens/lapt_logo.jpg`, `./assets/imagens/bsop_logo.png`, `./assets/imagens/ksop_logo.jpg`]
 
-function nextImage(){
-    count++;
-    if (count>4){
-        count = 1;
-        
+var efeitoCarrossel= document.getElementById('carrossel')
+
+carrossel.src= listaEventos[0];
+
+var i = 0;
+
+function proximo(){
+    if(i == 5){
+        i = -1;
     }
-    document.getElementById("radio" +count).checked = true;
+    i++
+    carrossel.src = listaEventos[i];
 }
 
+function anterior(){
+    if(i == 0){
+        i = 6;
+    }
+    i--
+    carrossel.src = listaEventos[i];
+}
