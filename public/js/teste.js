2bf0a78ecf24a8d563e76aa7ed6bda405f31ecfd
rike-lima <rodolfo.lima@sptech.school>
2024-05-27 19:19:33 -0300

    Alterações na home e inicio da lógica

--- public/js/teste.js
+++ public/js/teste.js
@@ -2,29 +2,18 @@ const nav = document.querySelector(".menuContent");
 
 window.addEventListener("scroll", () => nav.classList.toggle("active", scrollY > 0));
 
-var img1 = `./assets/imagens/WSOP_Logo.jpg`
+// function ativarMenu(){
+//     menuHamburguer.style.display='block'
+//     if(menuHamburguer.style.display=='block'){
+//         menuHamburguer.style.display='none'
+//     }
+// }
 
-
-var listaEventos=[img1, `./assets/imagens/ept_logo.jpg`,`./assets/imagens/PCA_logo.jpg`,`./assets/imagens/lapt_logo.jpg`, `./assets/imagens/bsop_logo.png`, `./assets/imagens/ksop_logo.jpg`]
-
-var efeitoCarrossel= document.getElementById('carrossel')
-
-carrossel.src= listaEventos[0];
-
-var i = 0;
-
-function proximo(){
-    if(i == 5){
-        i = -1;
+function ativarMenu() {
+    var menuHamburguer = document.getElementById('menuHamburguer');
+    if (menuHamburguer.style.display === 'none') {
+        menuHamburguer.style.display = 'block';
+    } else {
+        menuHamburguer.style.display = 'none';
     }
-    i++
-    carrossel.src = listaEventos[i];
-}
-
-function anterior(){
-    if(i == 0){
-        i = 6;
-    }
-    i--
-    carrossel.src = listaEventos[i];
 }
