9d314f915b5ca8a365690548d0a44e79e2ab43de
rike-lima <rodolfo.lima@sptech.school>
2024-06-04 19:49:17 -0300

    alterações na parte de gestão do BankRoll do usuário

--- database/scriptProjeto.sql
+++ database/scriptProjeto.sql
@@ -1,5 +1,4 @@
 use projetoPoker;
-
 describe usuario;
 
 select*from usuario;
@@ -15,3 +14,20 @@ insert into sites(nome) values
 ('Pokerstars'),
 ('GGPoker'),
 ('PartyPoker');
+
+create table bankroll (idBank int, fkConta int, primary key (idBank, fkConta), bankroll decimal(10,2) default 0
+);
+
+alter table bankroll add foreign key (fkConta) references conta(idConta);
+
+insert into conta values
+(1,1,1,'ikeda');
+
+insert	into bankroll values 
+(1,1,0);
+
+select bankroll from bankroll where fkConta = 1;
+
+update bankroll set bankroll = 150 where fkConta = 1;
+
+alter table usuario add column saldo decimal(10,2) default 0;
--- public/js/sessao.js
+++ public/js/sessao.js
@@ -2,11 +2,12 @@
 function validarSessao() {
     var email = sessionStorage.EMAIL_USUARIO;
     var nome = sessionStorage.NOME_USUARIO;
+    var nickname = sessionStorage.NICKNAME_USUARIO;
 
     var b_usuario = document.getElementById("b_usuario");
 
     if (email != null && nome != null) {
-        b_usuario.innerHTML = nome;
+        b_usuario.innerHTML = nickname;
     } else {
         window.location = "../login.html";
     }
