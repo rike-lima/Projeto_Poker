e0c9a3200cfad73926f5c0c5b421340fb22ff1e4
rike-lima <rodolfo.lima@sptech.school>
2024-06-10 23:23:39 -0300

    alterações no simulador, tentaiva de correção atávés de JSON

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,3 +1,12 @@
+function ativarMenu() {
+    var menuHamburguer = document.getElementById('menuHamburguer');
+    if (menuHamburguer.style.display === 'none') {
+        menuHamburguer.style.display = 'block';
+    } else {
+        menuHamburguer.style.display = 'none';
+    }
+}
+
 const $startGameButton = document.querySelector(".start_quiz");
 const $questionsContainer = document.querySelector(".questions_container");
 const $answersContainer = document.querySelector(".answers_container")
@@ -125,93 +134,93 @@ function finishGame() {
 
 const questions = [
     {
-        question: "Pergunta 1/10 - ",
+        question: "Pergunta 1/10 - Qual dessas combinações é a melhor? ",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false }
+            { text: "Straigth Flush", correct: false },
+            { text: "Royal Straigth Flush", correct: true },
+            { text: "Full House", correct: false },
+            { text: "Quadra", correct: false }
         ]
     },
     {
-        question: "Pergunta 2/10 - ",
+        question: "Pergunta 2/10 - Como se chama uma mão composta por cinco cartas todas do mesmo naipe?",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false }
+            { text: "Straight", correct: false },
+            { text: "Full House", correct: false },
+            { text: "Flush", correct: true },
+            { text: "River", correct: false }
         ]
     },
     {
-        question: "Pergunta 3/10 - ",
+        question: "Pergunta 3/10 - Quantas cartas de cada naipe há no baralho?",
         answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false }
+            { text: "13", correct: true },
+            { text: "12", correct: false },
+            { text: "14", correct: false },
+            { text: "11", correct: false }
         ]
     },
     {
-        question: "Pergunta 4/10 - ",
+        question: "Pergunta 4/10 - Qual o nome da aposta obrigatória que os jogadores devem colocar no inicio da rodada?",
         answers: [
-            { text: " ", correct: false },
-            { text: " ", correct: true },
-            { text: " ", correct: false },
-            { text: " ", correct: false }
+            { text: "Button ", correct: false },
+            { text: "Blind", correct: true },
+            { text: "Dealer", correct: false },
+            { text: "Under the Gun (UTG)", correct: false }
         ]
     },
     {
-        question: "Pergunta 5/10 - ",
+        question: "Pergunta 5/10 - Qual a mão vencedora no seguinte board: A♠ | K♠ | 2♥ | 3♣ | Q♠ ",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true }
+            { text: "A♥|A♦", correct: false },
+            { text: "J♠|T♥", correct: false },
+            { text: "4♦|5♠", correct: false },
+            { text: "T♠|9♠ ", correct: true }
         ]
     },
     {
-        question: "Pergunta 6/10 - ",
+        question: "Pergunta 6/10 -Qual a mão vencedora no seguinte board: A♠ | 2♠ | A♥ | A♣ | 7♠ ",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false }
+            { text: "2♦|3♦", correct: false },
+            { text: "T♠|9♠", correct: false },
+            { text: "8♥|7♣", correct: true },
+            { text: "2♥|2♣", correct: false }
         ]
     },
     {
-        question: "Pergunta 7/10 - ",
+        question: "Pergunta 7/10 - Qual o melhor combo de mão Pré-Flop ",
         answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false }
+            { text: "K|K", correct: false },
+            { text: "K|A", correct: false },
+            { text: "T|T", correct: false },
+            { text: "A|A", correct: true }
         ]
     },
     {
-        question: "Pergunta 8/10 - ",
+        question: "Pergunta 8/10 -Qual o nome de um board com 3 cartas comunitárias abertas: A♠ | K♠ | 2♥ ",
         answers: [
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false }
+            { text: "Flop", correct: true },
+            { text: "River", correct: false },
+            { text: "Turn", correct: false },
+            { text: "Dealer", correct: false }
         ]
     },
     {
-        question: "Pergunta 9/10 - ",
+        question: "Pergunta 9/10 - Qual a melhor opção abaixo que se refere aos esportes da mente?",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: false },
-            { text: "", correct: true }
+            { text: "Xadrez", correct: false },
+            { text: "Poker", correct: false },
+            { text: "Todas as opções", correct: true },
+            { text: "Bridge", correct: false }
         ]
     },
     {
-        question: "Pergunta 10/10 - ",
+        question: "Pergunta 10/10 - Qual a mão vencedora no seguinte board: K♠ | 2♠ | K♥ | A♣ | 3♠ ",
         answers: [
-            { text: "", correct: false },
-            { text: "", correct: true },
-            { text: "", correct: false },
-            { text: "", correct: false }
+            { text: "A♠|7♠", correct: false },
+            { text: "K♣|K♦", correct: true },
+            { text: "4♥|5♠", correct: false },
+            { text: "3♥|3♦", correct: false }
         ]
     },
 ]
--- public/js/simulador.js
+++ public/js/simulador.js
@@ -1,150 +1,168 @@
+var carta1 = [];
+var naipe1 = [];
+var carta2 = [];
+var naipe2 = [];
+var position = [];
+var resumoCombo = [];
+
+var maos_maximas = 0;
+var numero_telas = 0;
+
 function ativarMenu() {
-    var menuHamburguer = document.getElementById('menuHamburguer');
-    if (menuHamburguer.style.display = 'none') {
-        menuHamburguer.style.display = 'block';
-    } else {
-        menuHamburguer.style.display = 'none';
-    }
+  var menuHamburguer = document.getElementById("menuHamburguer");
+  if ((menuHamburguer.style.display = "none")) {
+    menuHamburguer.style.display = "block";
+  } else {
+    menuHamburguer.style.display = "none";
+  }
 }
 
 function simular() {
+  // simulador.style.display=none
 
-    // simulador.style.display=none
+  var maos_maximas = Number(ipt_config_maos.value);
+  var numero_telas = Number(ipt_config_telas.value);
 
-    var maos_maximas = ipt_config_maos.value;
-    var numero_telas = ipt_config_telas.value;
+  if (numero_telas == 0 || maos_maximas == 0) {
+    alert("Preencha os campos corretamente");
+  } else {
+    divContainer.style.display = "none";
 
-    if (numero_telas == 0 || maos_maximas == 0) {
-        alert('Preencha os campos corretamente')
-    } else {
-
-        divContainer.style.display = 'none';
-
-        if (numero_telas == 1) {
-            divTreinoUmaTela.style.display = "flex"
-        }
-
-        gerarMao()
+    if (numero_telas == 1) {
+      divTreinoUmaTela.style.display = "flex";
     }
+
+    gerarMao();
+  }
 }
-
-var carta1 = []
-var naipe1 = []
-var carta2 = []
-var naipe2 = []
-var position = []
-
 function gerarMao() {
- var primeira_carta= Math.round(Math.random()*12)+1;
- var segunda_carta= Math.round(Math.random()*12)+1;
- var primeiro_naipe= Math.round(Math.random()*3)+1;
- var segundo_naipe= Math.round(Math.random()*3)+1;
- var posicaoJogador= Math.round(Math.random()*5)+1;
+  var primeira_carta = Math.round(Math.random() * 12) + 1;
+  var segunda_carta = Math.round(Math.random() * 12) + 1;
+  var primeiro_naipe = Math.round(Math.random() * 3) + 1;
+  var segundo_naipe = Math.round(Math.random() * 3) + 1;
+  var posicaoJogador = Math.round(Math.random() * 5) + 1;
 
- if(primeira_carta==1){
-    carta1.push(A)
-} else if(primeira_carta==2){
+  if (primeira_carta == 1) {
     carta1.push(2);
-} else if(primeira_carta==3){
-    carta1.push(3)
-} else if(primeira_carta==4){
-    carta1.push(4)
-} else if(primeira_carta==5){
-    carta1.push(5)
-} else if(primeira_carta==6){
-    carta1.push(6)
-} else if(primeira_carta==7){
-    carta1.push(7)
-} else if(primeira_carta==8){
-    carta1.push(8)
-} else if(primeira_carta==9){
-    carta1.push(9)
-} else if(primeira_carta==10){
-    carta1.push(T)
-} else if(primeira_carta==11){
-    carta1.push(J)
-} else if(primeira_carta==12){
-    carta1.push(Q)
-} else {
-    carta1.push(K)
-} 
+  } else if (primeira_carta == 2) {
+    carta1.push(3);
+  } else if (primeira_carta == 3) {
+    carta1.push(4);
+  } else if (primeira_carta == 4) {
+    carta1.push(5);
+  } else if (primeira_carta == 5) {
+    carta1.push(6);
+  } else if (primeira_carta == 6) {
+    carta1.push(7);
+  } else if (primeira_carta == 7) {
+    carta1.push(8);
+  } else if (primeira_carta == 8) {
+    carta1.push(9);
+  } else if (primeira_carta == 9) {
+    carta1.push("T");
+  } else if (primeira_carta == 10) {
+    carta1.push("J");
+  } else if (primeira_carta == 11) {
+    carta1.push("Q");
+  } else if (primeira_carta == 12) {
+    carta1.push("K");
+  } else if (primeira_carta == 13) {
+    carta1.push("A");
+  }
 
-//geração da segunda carta:
+  //geração da segunda carta:
 
-if(segunda_carta==1){
-    carta2.push(A)
-} else if(segunda_carta==2){
-    carta2.push(2)
-} else if(segunda_carta==3){
-    carta2.push(3)
-} else if(segunda_carta==4){
-    carta2.push(4)
-} else if(segunda_carta==5){
-    carta2.push(5)
-} else if(segunda_carta==6){
-    carta2.push(6)
-} else if(segunda_carta==7){
-    carta2.push(7)
-} else if(segunda_carta==8){
-    carta2.push(8)
-} else if(segunda_carta==9){
-    carta2.push(9)
-} else if(segunda_carta==10){
-    carta2.push(T)
-} else if(segunda_carta==11){
-    carta2.push(J)
-} else if(segunda_carta==12){
-    carta2.push(Q)
-} else {
-    carta2.push(K)
-} 
+  if (segunda_carta == 1) {
+    carta2.push(2);
+  } else if (segunda_carta == 2) {
+    carta2.push(3);
+  } else if (segunda_carta == 3) {
+    carta2.push(4);
+  } else if (segunda_carta == 4) {
+    carta2.push(5);
+  } else if (segunda_carta == 5) {
+    carta2.push(6);
+  } else if (segunda_carta == 6) {
+    carta2.push(7);
+  } else if (segunda_carta == 7) {
+    carta2.push(8);
+  } else if (segunda_carta == 8) {
+    carta2.push(9);
+  } else if (segunda_carta == 9) {
+    carta2.push("T");
+  } else if (segunda_carta == 10) {
+    carta2.push("J");
+  } else if (segunda_carta == 11) {
+    carta2.push("Q");
+  } else if (segunda_carta == 12) {
+    carta2.push("K");
+  } else if (segunda_carta == 13) {
+    carta2.push("A");
+  }
+  //Geração Naipe
 
-//Geração Naipe
+  if (primeiro_naipe == 1) {
+    naipe1.push(`&#9824;`);
+  } else if (primeiro_naipe == 2) {
+    naipe1.push(`&#9829;`);
+  } else if (primeiro_naipe == 3) {
+    naipe1.push(`&#9827;`);
+  } else {
+    naipe1.push(`&#9830;`);
+  }
 
-if(primeiro_naipe==1){
-    naipe1.push(`&#9824;`)
-}else if(primeiro_naipe==2){
-    naipe1.push(`&#9829;`)
-} else if(primeiro_naipe==3){
-    naipe1.push(`&#9827;`)
-} 
-else {
-    naipe1.push(`&#9830;`)
-} 
+  if (segundo_naipe == 1) {
+    naipe2.push(`&#9824;`);
+  } else if (segundo_naipe == 2) {
+    naipe2.push(`&#9829;`);
+  } else if (segundo_naipe == 3) {
+    naipe2.push(`&#9827;`);
+  } else {
+    naipe2.push(`&#9830;`);
+  }
 
-if(segundo_naipe==1){
-    naipe2.push(`&#9824;`)
-}else if(segundo_naipe==2){
-    naipe2.push(`&#9829;`)
-} else if(segundo_naipe==3){
-    naipe2.push(`&#9827;`)
-} 
-else {
-    naipe2.push(`&#9830;`)
-} 
+  //gerar posição
 
-//gerar posição
+  if (posicaoJogador == 1) {
+    position.push("UTG");
+  } else if (posicaoJogador == 2) {
+    position.push("MP");
+  } else if (posicaoJogador == 3) {
+    position.push("HJ");
+  } else if (posicaoJogador == 4) {
+    position.push("CO");
+  } else if (posicaoJogador == 5) {
+    position.push("BTN");
+  } else {
+    position.push("SB");
+  }
 
-if(posicaoJogador==1){
-    position.push(UTG)
-}else if(posicaoJogador==2){
-    position.push(MP)
-} else if(posicaoJogador==3){
-    position.push(HJ)
-} else if(posicaoJogador==4){
-    position.push(CO)
-} else if(posicaoJogador==5){
-    position.push(BTN)
-} 
-else {
-    position.push(SB)
-} 
+  //   for (let i = 0; i < maos_maximas; i++) {
+  //     var posicaoAtual = position[i];
+  //     var cartaAtualUm = carta1[i];
+  //     var cartaAtualDois = carta2[i];
+  //     var naipeAtualUm = naipe1[i];
+  //     var naipeAtualDois = naipe2[i];
 
-posicaoJogador.innerHTML
-carta1.innerHTML
-carta2.innerHTML
-naipe1.innerHTML
-naipe2.innerHTML
+  //     id_posicaoJogador.innerHTML = posicaoAtual;
+  //     id_carta1.innerHTML = cartaAtualUm;
+  //     id_carta2.innerHTML = cartaAtualDois;
+  //     id_naipe1.innerHTML = naipeAtualUm;
+  //     id_naipe2.innerHTML = naipeAtualDois;
+  //     
+  //   }
 
-
+  id_posicaoJogador.innerHTML = position[0];
+  id_carta1.innerHTML = carta1[0];
+  id_carta2.innerHTML = carta2[0];
+  id_naipe1.innerHTML = naipe1[0];
+  id_naipe2.innerHTML = naipe2[0];
+  contadorRodada.innerHTML= 1
 }
+
+// var correcao = [
+//   {
+//     'posicao': 'utg',
+//     'maos': 'ATo'
+//   },
+// ];
